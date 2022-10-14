06309a367f29e13d7a978d20f551e1250d11dda1
Martin Novák <96317294+martinnovaak@users.noreply.github.com>
2022-10-14 21:09:30 +0200

    Add files via upload

---
+++ cv4/du4.cpp
@@ -0,0 +1,34 @@
+
+// Napiste funkci, ktera do konzole vypise "rovnostranny" trojuhelnik s poctem pater n 
+// pouzijte for cykly
+// uloha za 0,5 bodu
+void vypisTrojuhelnik(int n) // parametr n je pocet pater trojuhelniku
+{
+	/*
+	TODO
+	*/
+}
+
+// Napiste funkci, ktera spocte prvocisla od 2 do n, kde n je parametr funkce
+// uloha za 1 bod
+void eratosthen(bool poleCisel[], unsigned int n)
+{
+	/*
+	* TODO Erasthenuv algoritmus
+	* i-ty prvek pole poleCisel bude na konci true, pokud se jedna o prvocislo
+	*/
+
+	/*
+	* TODO
+	* vypiste cyklem prvocisla
+	*/
+}
+
+int main()
+{
+	const unsigned int N = 101;
+	bool cisla[N];
+	for (unsigned int i = 0; i < N; i++)
+		cisla[i] = true;
+	eratosthen(cisla, N);
+}
